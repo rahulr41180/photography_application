09fa15aea9be9bc11359c474ed000bb19e0835ff
Rahul Rathor <www.rahulr41180@gmail.com>
2024-04-29 16:32:26 +0530

    All Pages Update Done

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const menuBtn = document.getElementById("menu_btn");
 const navLinks = document.getElementById("nav_links");
 const menuBtnIcon = menuBtn.querySelector("svg");
 const closeBtnNavMenu = document.querySelector(".nav_menu_btn_bi-x");
-const slides = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll(".hero_slider1_slide");
 const prevButton = document.getElementById("prev_slide");
 const nextButton = document.getElementById("next_slide");
 const currentSlideIndicator = document.getElementById("current_slide");
 const nav2 = document.getElementById("nav2");
+
 
 menuBtn.addEventListener("click", (event) => {
     navLinks.classList.toggle("open");
@@ -16,7 +17,7 @@ menuBtn.addEventListener("click", (event) => {
 
     // menuBtnIcon.setAttribute("class", isOpen?"ri-close-line" : "ri-menu-line")
 
-    if(isOpen) {
+    if (isOpen) {
         closeBtnNavMenu.style.display = "block";
         menuBtnIcon.style.display = "none";
     } else {
@@ -32,23 +33,24 @@ navLinks.addEventListener("click", (event) => {
     menuBtnIcon.style.display = "block";
 })
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     let currentSlideIndex = 0;
 
     slides.forEach((slide, index) => {
-        slide.style.top = `${index * 100}%`;
+        slide.style.left = `${index * 100}%`;
     })
 
     function showSlide(index) {
 
         slides.forEach((slide, i) => {
-            slide.style.transform = `translateY(-${index * 100}%)`
+            slide.style.transform = `translateX(-${index * 100}%)`
         })
         currentSlideIndicator.textContent = (index + 1) + " / " + slides.length;
     }
 
     function prevSlide() {
+
         currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
         showSlide(currentSlideIndex);
 
@@ -65,9 +67,9 @@ document.addEventListener("DOMContentLoaded", function() {
     showSlide(currentSlideIndex);
 })
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     function scrollFunction() {
-        if(document.body.scrollTop > 372 || document.documentElement.scrollTop > 372) {
+        if (document.body.scrollTop > 372 || document.documentElement.scrollTop > 372) {
             nav2.style.top = "0";
         } else {
 
@@ -75,7 +77,72 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    window.onscroll = function() {
+    window.onscroll = function () {
         scrollFunction();
     }
+})
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const allSlides = document.querySelectorAll(".hero_slider2_carousel_item");
+    const prevBtn = document.getElementById("hero_slider2_prev");
+    const nextBtn = document.getElementById("hero_slider2_next");
+    const indicator = document.querySelector(".hero_slider2_indicator");
+    const tsmCurrent = document.querySelector(".tsm_current");
+    let currentIndex = 0;
+
+
+    function showIncdicator(index) {
+        indicator.textContent = (index + 1) + " / " + allSlides.length;
+        tsmCurrent.textContent = `TESTIMONIAL ${(index + 1)}`;
+    }
+
+    prevBtn.addEventListener("click", function () {
+        currentIndex = (currentIndex - 1 + allSlides.length) % allSlides.length;
+        showIncdicator(currentIndex);
+    })
+
+    nextBtn.addEventListener("click", function () {
+
+        currentIndex = (currentIndex + 1) % allSlides.length;
+        showIncdicator(currentIndex);
+    })
+    showIncdicator(currentIndex);
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    const allSlides = document.querySelectorAll(".hs3_slide");
+    const indicator = document.querySelector(".hs3_indicator");
+    const prevBtn = document.querySelector(".hs3_prev");
+    const nextBtn = document.querySelector(".hs3_next");
+    let currentIndex = 0;
+
+    var swiper = new Swiper(".mySwiper", {
+        cssMode: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        slidesPerView: "auto",
+        spaceBetween: 30,
+    });
+
+    function showIncdicator(index) {
+        indicator.textContent = (index + 1) + " / " + allSlides.length;
+    }
+
+    prevBtn.addEventListener("click", function () {
+        currentIndex = (currentIndex - 1 + allSlides.length) % allSlides.length;
+        showIncdicator(currentIndex);
+    })
+
+    nextBtn.addEventListener("click", function () {
+        currentIndex = (currentIndex + 1) % allSlides.length;
+        showIncdicator(currentIndex);
+    })
+
+    showIncdicator(currentIndex);
+
 })
